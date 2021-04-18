2c5d81827561a3c4be92d0137c1375908cc4ad5b
Arimah <arimah42@gmail.com>
2021-04-18 20:50:06 +0200

    Initial commit

---
+++ PerfectedLoop.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using R2API.Utils;
+using RoR2;
+
+#pragma warning disable IDE0051 // Remove unused private members
+
+namespace Arimah
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.arimah.PerfectedLoop", "PerfectedLoop", "1.0.0")]
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
+    public class PerfectedLoop : BaseUnityPlugin
+    {
+        public void Awake()
+		{
+            new Harmony("com.arimah.PerfectedLoop").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(CombatDirector))]
+    internal class CombatDirectorPatch
+	{
+        [HarmonyPostfix]
+        [HarmonyPatch("Init")]
+        private static void Init()
+        {
+            using var logger = Logger.CreateLogSource("PerfectedLoop");
+            var combatDirectorType = typeof(CombatDirector);
+            var eliteTiers = combatDirectorType.GetFieldValue<CombatDirector.EliteTierDef[]>("eliteTiers");
+            // Find the elite tier that contains Poison (Malachite) and Haunted (Celestine).
+            // This tier is used after looping.
+            var loopEliteTier = eliteTiers.FirstOrDefault(tier =>
+                tier.eliteTypes.Contains(RoR2Content.Elites.Poison) &&
+                tier.eliteTypes.Contains(RoR2Content.Elites.Haunted)
+            );
+            if (loopEliteTier != null)
+            {
+                loopEliteTier.eliteTypes = loopEliteTier.eliteTypes.Concat(new[] {
+                    RoR2Content.Elites.Lunar
+                }).ToArray();
+                logger.LogMessage("Perfected elites will now appear after looping");
+            }
+            else
+            {
+                logger.LogWarning("Could not find loop elite tiers");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# PerfectedLoop
+
+This mod makes [Perfected elites][perfected] spawn in the game after [looping][]. This elite type normally only spawns on the Moon. Perfected elites will spawn under the same conditions as Malachite and Celestine elites. As a result, Perfected, Malachite and Celestine elites will be approximately evenly distributed once they start appearing.
+
+![Perfected wisps facing the player](https://github.com/arimah/PerfectedLoop/blob/master/perfected_wisps.png?raw=true "Perfected wisps")
+
+In addition, Perfected elites will very occasionally drop [Shared Design][] when killed.
+
+![Shared Design item as a pickup](https://github.com/arimah/PerfectedLoop/blob/master/shared_design.png?raw=true "Shared Design")
+
+Perfected elites continue to spawn on the Moon.
+
+## Support
+
+If the mod fails to work, you can [open a GitHub issue][issue]. You can also contact me on Discord at Arimah#0001. I am _not_ in any Risk of Rain modding community.
+
+## Installation
+
+Use a mod manager such as [Thunderstore Mod Manager][r2mm] or copy `PerfectedLoop.dll` to your plugins folder.
+
+## License
+
+The mod is licensed under the MIT license. Please see the license text for the full terms.
+
+[perfected]: https://riskofrain2.fandom.com/wiki/Monsters#Perfected
+[looping]: https://riskofrain2.fandom.com/wiki/Environments#Looping
+[Shared Design]: https://riskofrain2.fandom.com/wiki/Shared_Design
+[issue]: https://github.com/arimah/PerfectedLoop/issues/new
+[r2mm]: https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager
